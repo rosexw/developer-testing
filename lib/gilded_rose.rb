f04a7fdb685e8e19a0b80e7875f2ae31870d466d
Rose Wan <rose.wan@me.com>
2017-09-06 19:39:08 -0400

    move sell_in to top, get a failed example

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,6 +12,11 @@ class GildedRose
 
     # cleaning up the code:
     @items.each do |item|
+      #WTF? why are we updating sell in here? #update_quality
+      if (item.name != "Sulfuras, Hand of Ragnaros")
+        item.sell_in = item.sell_in - 1;
+      end
+
       if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
         if (item.quality > 0)
           if (item.name != "Sulfuras, Hand of Ragnaros")
@@ -23,6 +28,9 @@ class GildedRose
         # iterate through the items
         # fix semicolons, setting to zero
         # how do we deal with new backstage passes, legendary, etc? no...it structures, not implements
+        # a lot of negative testing here...switch to positive testing (flip the if/else), don't use the keyword "unless"
+
+        # deal with items that don't degrade (and may improve) over time
         if (item.quality < 50)
           item.quality = item.quality + 1
           if (item.name == "Backstage passes to a TAFKAL80ETC concert")
@@ -39,10 +47,8 @@ class GildedRose
           end
         end
       end
-      if (item.name != "Sulfuras, Hand of Ragnaros")
-        item.sell_in = item.sell_in - 1;
-        # should be updating quality, not sell_in...
-      end
+      #sell_in update was here
+      # items degrade faster or altogether after sell_in reaches 0
       if (item.sell_in < 0)
         if (item.name != "Aged Brie")
           if (item.name != "Backstage passes to a TAFKAL80ETC concert")
@@ -52,8 +58,8 @@ class GildedRose
               end
             end
           else
-            #setting quality to zero in a bad way
-            item.quality = item.quality - item.quality
+            # instead of subtracting item.quality from item.quality, set it to 0.
+            item.quality = 0
           end
         else
           if (item.quality < 50)
